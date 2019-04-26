69c09fbd7cdda0599766d662f858522293a41f4c
ceelogre <sam.nishimwe@yahoo.com>
2019-04-26 21:16:50 +0200

    Initial setup. Show lat

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <button v-on:click="getLocation">Start loc</button>
+    <h2> {{ geo}} </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      geo: null
+    };
+  },
+  methods: {
+    getLocation() {
+      if(navigator.geolocation){
+        this.geo = navigator.geolocation.getCurrentPosition( x => {
+          this.geo = x.coords.latitude,
+          alert('Val')
+        }, error => { alert('ERRR', error)})
+      }
+    },
+    askGeo(){
+      if("geolocation" in navigator){
+        alert('Yes')
+      }
+      else{
+        alert('No');
+      }
+    },
+  }
+}
+</script>
